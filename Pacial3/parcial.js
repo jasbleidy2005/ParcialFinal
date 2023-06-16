e397b209aca58e4fa7e0b1602f6cc0c5e08c4594
jasbleidy2005 <132091094+jasbleidy2005@users.noreply.github.com>
2023-06-16 11:09:30 -0500

    Add files via upload

---
+++ Pacial3/parcial.js
@@ -0,0 +1,33 @@
+let objetos_escolares = [{
+
+"nombre": "lapiz",
+"precio": 2000,
+"color": "rojo",
+"tamaño": "M",
+"cantidad": 1
+},
+
+{
+"nombre": "lapicero",
+"precio": 3000,
+"color": "azul",
+"tamaño": "L",
+"cantidad": 3
+},
+
+{
+"nombre": "papel",
+"precio": 400,
+"color": "blanco",
+"tamaño": "A4",
+"cantidad": 5
+}, {
+"nombre": "borrador",
+"precio": 1400,
+"color": "negro",
+"tamaño": "S",
+"cantidad": 2
+}]
+
+
+console.log(objetos_escolares);
